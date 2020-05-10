7012edb49ac274976b2b8a12fb64480b5a74b1ca
Madbrotherz <61371417+Madbrotherz@users.noreply.github.com>
2020-05-10 17:33:33 +0700

    Commit & Push to Master Branch

---
+++ Assets/Scripts/PowerupHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupHealth : MonoBehaviour
+{
+    GameObject player;
+    PlayerHealth playerHealthScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerHealthScript = GameObject.Find("Player").GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (playerHealthScript.currentHealth < 100)
+            {
+                Debug.Log("Health is Increased");
+                playerHealthScript.currentHealth += 30;
+                playerHealthScript.healthSlider.value = playerHealthScript.currentHealth;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PowerupSpeed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpeed : MonoBehaviour
+{
+    GameObject player;
+    public GameObject particleSpeed;
+    PlayerMovement playerMovementScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerMovementScript = GameObject.Find("Player").GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Speed is Increased");
+            playerMovementScript.speed = 10;
+            particleSpeed.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
